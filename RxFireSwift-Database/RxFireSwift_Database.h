400262d4ad4ee6c5f5f126323f5a5d826b90bbbb
Morten Bek Ditlevsen <morten@ka-ching.dk>
2018-10-01 08:09:18 +0200

    Changed into a static framework

---
+++ RxFireSwift-Database/RxFireSwift_Database.h
@@ -0,0 +1,19 @@
+//
+//  RxFireSwift_Database.h
+//  RxFireSwift-Database
+//
+//  Created by Morten Bek Ditlevsen on 01/10/2018.
+//  Copyright © 2018 Ka-ching. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxFireSwift_Database.
+FOUNDATION_EXPORT double RxFireSwift_DatabaseVersionNumber;
+
+//! Project version string for RxFireSwift_Database.
+FOUNDATION_EXPORT const unsigned char RxFireSwift_DatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxFireSwift_Database/PublicHeader.h>
+
+
